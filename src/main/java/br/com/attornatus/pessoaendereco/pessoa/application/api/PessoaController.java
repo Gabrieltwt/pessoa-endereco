8532fdb75ad72c298b35bbbe4ef5b5fa948eb587
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-10-14 22:51:33 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/api/PessoaController.java
+++ src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/api/PessoaController.java
@@ -2,17 +2,22 @@ package br.com.attornatus.pessoaendereco.pessoa.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.attornatus.pessoaendereco.pessoa.application.service.PessoaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PessoaController implements PessoaAPI {
+	private final PessoaService pessoaService;
 
 	@Override
 	public PessoaResponse postPessoa(PessoaRequest pessoaRequest) {
 		log.info("[inicia] PessoaController - postPessoa");
+		PessoaResponse pessoaCriada = pessoaService.criaPessoa(pessoaRequest);
 		log.info("[finaliza] PessoaController - postPessoa");
-		return null;
+		return pessoaCriada;
 	}
 
 }
