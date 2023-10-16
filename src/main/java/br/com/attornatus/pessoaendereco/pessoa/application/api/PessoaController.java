fb4649f81deb8922db8b14529c2f71cd19a2689e
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-10-16 17:58:01 -0300

    implementa fluxo api, teste ok!

--- src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/api/PessoaController.java
+++ src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/api/PessoaController.java
@@ -1,5 +1,7 @@
 package br.com.attornatus.pessoaendereco.pessoa.application.api;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.attornatus.pessoaendereco.pessoa.application.service.PessoaService;
@@ -18,6 +20,13 @@ public class PessoaController implements PessoaAPI {
 		PessoaResponse pessoaCriada = pessoaService.criaPessoa(pessoaRequest);
 		log.info("[finaliza] PessoaController - postPessoa");
 		return pessoaCriada;
+	}
+
+	@Override
+	public List<PessoaListResponse> getTodasPessoas() {
+		log.info("[inicia] PessoaController - getTodasPessoas");
+		log.info("[finaliza] PessoaController - getTodasPessoas");
+		return null;
 	}
 
 }
---
+++ src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/api/PessoaListResponse.java
@@ -0,0 +1,8 @@
+package br.com.attornatus.pessoaendereco.pessoa.application.api;
+
+import java.util.UUID;
+
+public class PessoaListResponse {
+	private UUID idPessoa;
+	private String nomeCompleto;
+}
