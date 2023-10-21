d65d3c6650fdd1704dfb19e3b682eb0bdd51b65f
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-10-21 19:57:29 -0300

    implementa fluxo de servico de aplicacao

--- src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/service/PessoaApplicationService.java
+++ src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/service/PessoaApplicationService.java
@@ -1,9 +1,11 @@
 package br.com.attornatus.pessoaendereco.pessoa.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.attornatus.pessoaendereco.pessoa.application.api.PessoaDetalhadoResponse;
 import br.com.attornatus.pessoaendereco.pessoa.application.api.PessoaListResponse;
 import br.com.attornatus.pessoaendereco.pessoa.application.api.PessoaRequest;
 import br.com.attornatus.pessoaendereco.pessoa.application.api.PessoaResponse;
@@ -32,5 +34,12 @@ public class PessoaApplicationService implements PessoaService {
 		List<Pessoa> pessoas = pessoaRepository.buscaTodasPessoas();
 		log.info("[finaliza] PessoaApplicationService - buscaTodasPessoas");
 		return PessoaListResponse.converte(pessoas);
+	}
+
+	@Override
+	public PessoaDetalhadoResponse buscaPessoaAtravesId(UUID idPessoa) {
+		log.info("[inicia] PessoaApplicationService - buscaPessoaAtravesId");
+		log.info("[finaliza] PessoaApplicationService - buscaPessoaAtravesId");
+		return null;
 	}
 }
